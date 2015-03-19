65c0765a3a4e137ec375697c43a19fe733468ba0
Glenn Gillen <me@glenngillen.com>
2015-03-19 21:37:22 +1100

    We don't need a BUILDKITE_BIN_PATH.

--- etc/bootstrap.sh
+++ etc/bootstrap.sh
@@ -107,9 +107,6 @@ function buildkite-local-hook {
 #
 ##############################################################
 
-# Add the $BUILDKITE_BIN_PATH to the $PATH
-export PATH="$BUILDKITE_BIN_PATH:$PATH"
-
 # Come up with the place that the repository will be checked out to
 SANITIZED_AGENT_NAME=$(echo $BUILDKITE_AGENT_NAME | tr -d '"')
 PROJECT_FOLDER_NAME="$SANITIZED_AGENT_NAME/$BUILDKITE_PROJECT_SLUG"
